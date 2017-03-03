6e3c3906928eb1f49703e581c96e06856021eaf8
Camilo Diaz <info@enteraweb.co>
2017-03-02 23:43:37 -0500

    Cabecera del sitio web
    modificar el menu

---
+++ header.php
@@ -0,0 +1,89 @@
+            <header id="Header">
+
+                <!-- Header -  Logo and Menu area -->
+                <div id="Top_bar">
+                    <div class="container">
+                        <div class="column one">
+                            <div class="top_bar_left clearfix">
+                                <!-- Logo-->
+                                <div class="logo">
+                                    <h1><a id="logo" href="index.php" title="BePolitics - BeTheme"><img class="scale-with-grid" src="content/politics/images/politics.png" alt="BePolitics - BeTheme" /></a></h1>
+                                </div>
+                                <!-- Main menu-->
+                                <div class="menu_wrapper">
+                                    <nav id="menu">
+                                        <ul id="menu-main-menu" class="menu">
+                                            <li class=" current_page_item">
+                                                <a href="index.php"><span> Home </span></a>
+                                            </li>
+                                            <li>
+                                                <a href="content/politics/news.html"><span>News</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="content/politics/important-subjects.html"><span>Important subjects</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="content/politics/about.html"><span>About</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="content/politics/documents.html"><span>Documents</span></a>
+                                            </li>
+                                            <li>
+                                                <a href="content/politics/staff.html"><span>Staff</span></a>
+                                            </li>
+                                        </ul>
+                                    </nav><a class="responsive-menu-toggle " href="#"><i class="icon-menu"></i></a>
+                                </div>
+                                <!-- Header Searchform area-->
+                                <div class="search_wrapper">
+                                    <form method="get" action="#">
+                                        <i class="icon_search icon-search"></i><a href="#" class="icon_close"><i class="icon-cancel"></i></a>
+                                        <input type="text" class="field" name="s" placeholder="Enter your search" />
+                                        <input type="submit" class="submit flv_disp_none" value="" />
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Revolution slider area-->
+                <div class="mfn-main-slider">
+                    <div id="rev_slider_1_2_wrapper" class="rev_slider_wrapper fullwidthbanner-container" style="margin:0px auto;padding:0px;margin-top:0px;margin-bottom:0px;max-height:670px;">
+                        <div id="rev_slider_1_2" class="rev_slider fullwidthabanner" style="display:none;max-height:670px;height:670px;">
+                            <ul>
+                                <li data-transition="fade" data-slotamount="7" data-masterspeed="300" data-saveperformance="off">
+                                    <img src="content/politics/images/home_politics_slide1.jpg" alt="home_politics_slide1" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+                                    <div class="tp-caption mfnrspoliticslargewhite sfb tp-resizeme" data-x="center" data-hoffset="0" data-y="center" data-voffset="-100" data-speed="900" data-start="500" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300" style="z-index: 5; max-width: auto; max-height: auto; white-space: nowrap;">
+                                        Dapibus risus ante sodales lectus
+                                        <br /> blandit eu tempor
+                                    </div>
+                                    <div class="tp-caption mfnrspoliticsmediumwhite sfb tp-resizeme" data-x="center" data-hoffset="0" data-y="center" data-voffset="40" data-speed="900" data-start="1200" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300" style="z-index: 6; max-width: auto; max-height: auto; white-space: nowrap;">
+                                        Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada
+                                        <br /> ultricies curabitur et ligula ut molestie
+                                    </div>
+                                    <div class="tp-caption sfb" data-x="center" data-hoffset="0" data-y="center" data-voffset="160" data-speed="900" data-start="1800" data-easing="Power3.easeInOut" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300" style="z-index: 7;">
+                                        <a href="#"><img src="content/politics/images/home_politics_button_slider.png" alt="">
+                                        </a>
+                                    </div>
+                                </li>
+                                <li data-transition="fade" data-slotamount="7" data-masterspeed="300" data-saveperformance="off">
+                                    <img src="content/politics/images/home_politics_slide2.jpg" alt="home_politics_slide2" data-bgposition="center top" data-bgfit="cover" data-bgrepeat="no-repeat">
+                                    <div class="tp-caption mfnrspoliticslargewhite sfb tp-resizeme" data-x="center" data-hoffset="0" data-y="center" data-voffset="-100" data-speed="900" data-start="500" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300" style="z-index: 5; max-width: auto; max-height: auto; white-space: nowrap;">
+                                        Aliquam sem in hendrerit nulla
+                                        <br /> quam nunc accumsan
+                                    </div>
+                                    <div class="tp-caption mfnrspoliticsmediumwhite sfb tp-resizeme" data-x="center" data-hoffset="0" data-y="center" data-voffset="40" data-speed="900" data-start="1200" data-easing="Power3.easeInOut" data-splitin="none" data-splitout="none" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300" style="z-index: 6; max-width: auto; max-height: auto; white-space: nowrap;">
+                                        Suspendisse a pellentesque dui, non felis. Maecenas malesuada elit lectus felis, malesuada
+                                        <br /> ultricies curabitur et ligula ut molestie
+                                    </div>
+                                    <div class="tp-caption sfb" data-x="center" data-hoffset="0" data-y="center" data-voffset="160" data-speed="900" data-start="1800" data-easing="Power3.easeInOut" data-elementdelay="0.1" data-endelementdelay="0.1" data-endspeed="300" style="z-index: 7;">
+                                        <a href="#"><img src="content/politics/images/home_politics_button_slider.png" alt="">
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="tp-bannertimer tp-bottom flv_viz_hid"></div>
+                        </div>
+                    </div>
+                </div>
+            </header>
