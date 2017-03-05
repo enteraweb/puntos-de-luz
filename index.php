ed0f80974d1705a2a6d49fee4c9f25301d284df4
Camilo Diaz <info@enteraweb.co>
2017-03-05 11:13:39 -0500

    Revert "error"
    This reverts commit 550831374a2e02ec55aded88196eaee88727aa0d.

--- index.php
+++ index.php
@@ -511,65 +511,7 @@
             </div>
         </div>
         <!-- Footer-->
-        <footer id="Footer" class="clearfix">
-            <div class="widgets_wrapper">
-                <div class="container">
-                    <div class="one column">
-                        <!-- Text Area -->
-                        <aside id="text-2" class="widget widget_text">
-                            <div class="textwidget">
-                                <!-- One Fourth (1/4) Column -->
-                                <div class="column one-fourth">
-                                    <div class="image_frame no_link scale-with-grid no_border aligncenter">
-                                        <div class="image_wrapper"><img class="scale-with-grid" src="content/politics/images/home_politics_logo.png" alt="" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- Three Fourth (3/4) Column -->
-                                <div class="column three-fourth">
-                                    <ul class="flv_list_ul_10">
-                                        <li class="flv_list_1">
-                                            <a href="#">Home</a>
-                                        </li>
-                                        <li class="flv_list_1">
-                                            <a href="#">News</a>
-                                        </li>
-                                        <li class="flv_list_1">
-                                            <a href="#">Important subjects</a>
-                                        </li>
-                                        <li class="flv_list_1">
-                                            <a href="#">About</a>
-                                        </li>
-                                        <li class="flv_list_1">
-                                            <a href="#">Documents</a>
-                                        </li>
-                                        <li class="flv_list_1">
-                                            <a href="#">Staff</a>
-                                        </li>
-                                    </ul>
-                                    <p style="margin: 0 0 0 10px;">
-                                        &copy; 2014 BeBePolitics - BeTheme. Muffin group - HTML by <a target="_blank" rel="nofollow" href="http://themeforest.net/item/betheme-html-responsive-multipurpose-template/13925633?ref=beantownthemes">BeantownThemes</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </aside>
-                    </div>
-                </div>
-            </div>
-            <!-- Footer copyright-->
-            <div class="footer_copy">
-                <div class="container">
-                    <div class="column one">
-                        <a id="back_to_top" href="#" class="button button_left button_js"><span class="button_icon"><i class="icon-up-open-big"></i></span></a>
-                        <div class="copyright">
-                            &copy; 2015 BePolitics - BeTheme. Muffin group - HTML by <a target="_blank" rel="nofollow" href="http://themeforest.net/item/betheme-html-responsive-multipurpose-template/13925633?ref=beantownthemes">BeantownThemes</a>
-                        </div>
-                        <!--Social info area-->
-                        <ul class="social"></ul>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <?php include('footer.php'); ?>
     </div>
     <!-- JS -->
 
